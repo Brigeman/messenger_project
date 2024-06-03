53cd206a4b44309d505235cc5c6fbcbb9dbe62e9
Brigeman <yurgen.brigeman@mail.ru>
2024-06-03 10:48:27 +0200

    viewset, url, settings

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChatViewSet, MessageViewSet, index
+from .views import ChatViewSet, MessageViewSet, UserViewSet, index
 
 router = DefaultRouter()
 router.register(r"chats", ChatViewSet)
 router.register(r"messages", MessageViewSet)
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", index, name="index"),
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 from .models import Chat, Message
-from .serializers import ChatSerializer, MessageSerializer
+from .serializers import ChatSerializer, MessageSerializer, UserSerializer
 from django.shortcuts import render
 
 
@@ -8,10 +9,23 @@ class ChatViewSet(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
+    def perform_create(self, serializer):
+        chat = serializer.save()
+        chat.members.add(self.request.user)
+        chat.save()
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 def index(request):
