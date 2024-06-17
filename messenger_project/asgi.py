f5c16e3f49dc1ccaee61d5ffa77458749d9b3a09
Brigeman <yurgen.brigeman@mail.ru>
2024-06-17 15:32:28 +0200

    template for privat chat

--- messenger_project/asgi.py
+++ messenger_project/asgi.py
@@ -1,14 +1,14 @@
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messenger_project.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
